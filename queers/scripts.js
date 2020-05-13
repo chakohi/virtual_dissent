ff601e1bfc506691c8fd1267d7c31e26bf51292c
Adham Chakohi <swagbook@swagbook.local>
2020-05-14 00:02:45 +0400

    scaling

--- queers/scripts.js
+++ queers/scripts.js
@@ -2,6 +2,7 @@ document.querySelector(".start").addEventListener("click", (e) => {
     console.log("hello")
     let x = document.querySelector(".start");
     let y = document.querySelector(".credits");
+    document.querySelector("#footer").style.position = "relative";
     document.querySelector(".start").parentNode.removeChild(x);
     document.querySelector(".credits").parentNode.removeChild(y);
     document.querySelector(".second").style.visibility = 'visible';
